8675a63e4bd6db45168465aad19e47f895d02b85
Mary Kathryn <msmarykathrynholt@gmail.com>
2020-09-27 19:15:15 -0500

    Start to link JS to HTML & Created 2nd JS

---
+++ script.js
@@ -0,0 +1,9 @@
+var firstBox = document.getElementById("guide");
+var secondBox = document.getElementById("container");
+var thirdBox = document.getElementById("finish");
+var 
+
+
+secondBox.addEventListener("click", function(event) {
+
+})
