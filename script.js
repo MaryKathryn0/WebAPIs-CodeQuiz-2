c33dbd84162e6b0eafe1a8b4b801044cf999cc07
Mary Kathryn <msmarykathrynholt@gmail.com>
2020-09-29 06:56:47 -0500

    moved file

--- script.js
+++
@@ -1,9 +0,0 @@
-var firstBox = document.getElementById("guide");
-var secondBox = document.getElementById("container");
-var thirdBox = document.getElementById("finish");
-var 
-
-
-secondBox.addEventListener("click", function(event) {
-
-})
