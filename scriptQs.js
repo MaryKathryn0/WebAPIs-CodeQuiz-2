d8b5aaded43535fd54b6e8abb443598bff56b464
Mary Kathryn <msmarykathrynholt@gmail.com>
2020-09-29 08:43:46 -0500

    moved all onclick stuff together

--- scriptQs.js
+++ scriptQs.js
@@ -28,7 +28,7 @@ var qOptions5 = ["JavaScript", "terminal/bash", "for loops", "console.log"];
 //}
 
 // Change guide to actual question. 
-var guess = document.querySelector("choices");
+
 // targetDiv.textsContent = qOptions1
 var response = ["4"]; // for end at bottom
 var grader = document.querySelector(".grader");
@@ -46,13 +46,13 @@ var choice4 = document.getElementById("choice4");
 var choices = document.querySelector(".choices");
 // var q1Length = qOptions1.length;
 
-// var end = document - last question???
+
 
 // Make Guide disappear on click of start and Question 1 appear
 //Guide to Q1
 start.addEventListener("click", function () {
     content.removeChild(guide);
-    qBox.style.display="block";
+    qBox.style.display = "block";
     question1()
 })
 function question1() {
@@ -65,105 +65,59 @@ function question1() {
 
     }
 }
-choices.addEventListener("click", function() {
-    if(allQuestions[0]&& this.data-index===3){
-correct.style.display = "block"
-question2()
-    }
-    else if(allQuestions[0]&& this.data-index !==3) {
-wrong.style.display = "block"
-question2()
-    }
-
-})
-
-
-// Make Question 1 removed and for Question 2 appear
-function question2() {
-    setQuestion(1);
-    for (let i = 0; i < qOptions1.length; i++) {
-        choice1.innerText = qOptions2[0];
-        choice2.innerText = qOptions2[1];
-        choice3.innerText = qOptions2[2];
-        choice4.innerText = qOptions2[3];
-    }
-}
-choices.addEventListener("click", function() {
-    if(allQuestions[0]&& this.data-index===3) {
-        correct.style.display = "block"
-        question3()
-    }
-    else if(allQuestions[0]&& this.data-index !==3) {
-        wrong.style.display = "block"
-        question3()
-    }
-})
-// Question 3
-function question3() {
-    setQuestion(2);
-    for (let i = 0; i < qOptions1.length; i++) {
-        choice1.innerText = qOptions3[0];
-        choice2.innerText = qOptions3[1];
-        choice3.innerText = qOptions3[2];
-        choice4.innerText = qOptions3[3];
-    }
-}
-choices.addEventListener("click", function() {
-    if(allQuestions[0]&& this.data-index ===1) {
-        correct.style.display = "block"
-        question4()
-    }
-    else if(allQuestions[0]&& this.data-index !==1) {
-        wrong.style.display = "block"
-        question4()
-    }
-})
-    
-// Question 4
-function question4() {
-    setQuestion(3);
-    for (let i = 0; i < qOptions1.length; i++) {
-        choice1.innerText = qOptions4[0];
-        choice2.innerText = qOptions4[1];
-        choice3.innerText = qOptions4[2];
-        choice4.innerText = qOptions4[3];
-    }
-}
 choices.addEventListener("click", function () {
-    if(allQuestions[0]&& this.data-index ===3) {
+    if (allQuestions[0] && this.data-index == 3) {
+        correct.style.display = "block"
+        question2()
+    }
+    else if (allQuestions[0] && this.data - index !== 3) {
+        wrong.style.display = "block"
+        question2()
+    }
+    //Q2 EL
+    if (allQuestions[1] && this.data - index === 3) {
+        correct.style.display = "block"
+        question3()
+    }
+    else if (allQuestions[1] && this.data - index !== 3) {
+        wrong.style.display = "block"
+        question3()
+    }
+    //Q3 EL
+    if (allQuestions[2] && this.data - index === 1) {
+        correct.style.display = "block"
+        question4()
+    }
+    else if (allQuestions[2] && this.data - index !== 1) {
+        wrong.style.display = "block"
+        question4()
+    }
+    //Q4 EL
+    if (allQuestions[3] && this.data - index === 3) {
         correct.style.display = "block"
         question5()
     }
-    else if(allQuestions[0]&& this.data-index !==3) {
+    else if (allQuestions[3] && this.data - index !== 3) {
         wrong.style.display = "block"
         question5()
     }
-})
-// Question 5
-function question5() {
-    setQuestion(4);
-    for (let i = 0; i < qOptions1.length; i++) {
-        choice1.innerText = qOptions5[0];
-        choice2.innerText = qOptions5[1];
-        choice3.innerText = qOptions5[2];
-        choice4.innerText = qOptions5[3];
+    //Q5 EL
+    if (allQuestions[4] && this.data - index === 4) {
+        correct.style.display = "block"
+
     }
-}
-    choices.addEventListener("click", function () {
-        if(allQuestions[0]&& this.data-index ===4) {
-            correct.style.display = "block"
-            
-        }
-        else if(allQuestions[0]&& this.data-index !==4) {
-            wrong.style.display = "block"
-            
-        }
+    else if (allQuestions[4] && this.data - index !== 4) {
+        wrong.style.display = "block"
+
+    }
+
 })
+
 
 
 // Make qBox go away on click of last question *** DO WHAT I DID FOR qBox***
-choice4.addEventListener("click", function() {
+choice4.addEventListener("click", function () {
     content.removeChild(qBox);
-    finish.style.display="block";
+    finish.style.display = "block";
     console.log("click");
 })
